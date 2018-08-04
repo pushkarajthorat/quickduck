9aef438d3ab79e1ec856763ea07c0c3087dc61b7
Pushkaraj Thorat <pushkarajthorat@gmail.com>
2018-08-04 13:14:11 +0530

    first commit

---
+++ DateTick/SolvePerm.py
@@ -0,0 +1,32 @@
+
+# Index location
+# |       | one   | two   |     |
+# | three | four  | five  | six |
+# |       | seven | eight |     |
+
+import itertools
+
+array = [1,2,3,4,5,6,7,8]
+adjecent = [(1,4),(4,7),(2,5),(5,8),(1,2),(3,4),(4,5),(5,6),(7,8)]
+
+adjecent_normalized = [ (x - 1, y - 1) for (x, y) in adjecent]
+
+def validate(perm, adjecent):
+    for (x, y) in adjecent:
+        if abs(perm[x] - perm[y]) == 1: return False
+    return True
+
+def print_permutation(perm):
+    print """
+    |   | {} | {} |   |
+    | {} | {} | {} | {} |
+    |   | {} | {} |   |
+    """.format(*perm)
+
+total_solutions=0
+for perm in itertools.permutations(array):
+    if validate(perm, adjecent_normalized):
+        print_permutation(perm)
+        total_solutions += 1
+
+print "total_solutions {}".format(total_solutions) 
