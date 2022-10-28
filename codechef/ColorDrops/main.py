55b60ec81238b4243b99d1d783be972825955119
Pushkaraj Thorat <pushkaraj.thorat@in.ibm.com>
2022-10-28 18:41:09 +0530

    code chef - color drops

--- codechef/ColorDrops/main.py
+++ codechef/ColorDrops/main.py
@@ -7,26 +7,20 @@ import math
 
 
 class color():
-
     def __init__(self, r, g, b):
         self.r = r
         self.g = g
         self.b = b
         self.propotion = tuple(self.get_propotion())
-
     def get_propotion(self):
         gcd = math.gcd(self.r, self.g, self.b)
         return map(lambda x: x / gcd, [self.r, self.g, self.b])
-
     def __eq__(self, other):
         return self.propotion == other.propotion
-
     def __ne__(self, other):
         return self.propotion != other.propotion
-
     def __hash__(self):
         return self.propotion.__hash__()
-
     def __repr__(self):
         return f"\n({self.r},{self.g},{self.b})"
 
@@ -40,7 +34,7 @@ def valid_color(r, g, b):
         l.sort(reverse=True)
         if l[0] == 1:
             return True
-        return False
+    return False
 
 
 def find_color_combinations(red, green, blue):
@@ -52,18 +46,25 @@ def find_color_combinations(red, green, blue):
                     c = color(i, j, k)
                     if c not in color_set:
                         color_set.append(c);
+                        # yield c
+    color_set.sort(key=lambda x:x.r+x.g+x.b)
     return color_set
 
+cache = {}
 def find_longest_path(r, g, b, used_colors=[]):
     choosen_color_sequence = []
+    if (r, g, b) in cache:
+        # print("found in cache")
+        return cache[(r, g, b)]
+
     for c in find_color_combinations(r, g, b):
         if c not in used_colors:
             max_color_sequence = find_longest_path(r - c.r, g - c.g, b - c.b, used_colors+[c])
             max_color_sequence = [c] + max_color_sequence
             if len(choosen_color_sequence) < len(max_color_sequence):
                 choosen_color_sequence = max_color_sequence
+    cache[(r, g, b)]=choosen_color_sequence[-1:]
     return choosen_color_sequence
 
-
-print(find_longest_path(5, 4, 4, []))
-
+print(find_longest_path(20,20,20, []))
+# print(cache)
